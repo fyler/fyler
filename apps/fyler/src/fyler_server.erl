fb74da0ec3fd24426d5e2e1a8b9b5412a0faa3a7
palkan <dementiev@webils.ru>
2013-09-17 14:46:07 +0400

    fix bug with missing  atom_to_list

--- apps/fyler/src/fyler_server.erl
+++ apps/fyler/src/fyler_server.erl
@@ -191,7 +191,7 @@ send_response(#task{callback = undefined},_,_) ->
 
 
 send_response(#task{callback = Callback, file=#file{is_aws = AWS}},#job_stats{result_path = Path},success) ->
-  ibrowse:send_req(binary_to_list(Callback),[],post,"status=ok&aws="++AWS++"&data="++jiffy:encode({[{path,Path}]}),[]);
+  ibrowse:send_req(binary_to_list(Callback),[],post,"status=ok&aws="++atom_to_list(AWS)++"&data="++jiffy:encode({[{path,Path}]}),[]);
 
 send_response(#task{callback = Callback},_,failed) ->
   ibrowse:send_req(binary_to_list(Callback),[],post,"status=failed",[]).
