04bce610cc548cfa80b2c70dbe29e2f172fb95b1
palkan <dementiev@webils.ru>
2014-02-19 11:15:17 +0400

    fix failed task to pg record

--- apps/fyler/src/fyler_utils.erl
+++ apps/fyler/src/fyler_utils.erl
@@ -34,7 +34,7 @@ stats_to_pg_string(#job_stats{status=Status,
 
   ResultsList = ulitos:join([binary_to_list(R) || R<-Results],","),
 
-  "'"++atom_to_list(Status)++"',"++integer_to_list(DTime)++","++integer_to_list(UTime)++","++integer_to_list(Size)++",'"++Path++"',"++integer_to_list(Time)++",'"++ResultsList++"','"++atom_to_list(Type)++"','Error'".
+  "'"++atom_to_list(Status)++"',"++integer_to_list(to_int(DTime))++","++integer_to_list(to_int(UTime))++","++integer_to_list(to_int(Size))++",'"++Path++"',"++integer_to_list(to_int(Time))++",'"++ResultsList++"','"++atom_to_list(Type)++"','Error'".
 
 
 %% @doc Convert epgsql time fromat to number.
@@ -55,6 +55,10 @@ pgtime_to_string(_F) -> {error,badformat,_F}.
 pad(N) when N < 10 -> "0"++integer_to_list(N);
 
 pad(N) -> integer_to_list(N).
+
+to_int(undefined) -> 0;
+
+to_int(N) -> N.
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
