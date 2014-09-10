f0d079ea4c5a1bfc58916413483a0f1a00684465
silkbottle <silkbottle@gmail.com>
2014-09-10 18:16:30 +0400

    add media to handlers

--- apps/fyler/include/handlers.hrl
+++ apps/fyler/include/handlers.hrl
@@ -1,5 +1,5 @@
 -define(Handlers,[video_to_mp4,video_to_hls,video_thumb,video_probe,split_pdf,
-                  recording_to_hls,pdf_to_thumbs,pdf_to_swf_thumbs,pdf_to_swf,
-                  pdf_to_pages,pdf_thumb,pdf_split_pages,image_thumb,
-                  doc_to_pdf_thumbs,doc_to_pdf_swf,doc_to_pdf,do_nothing,
-                  audio_to_mp3]).
+                  recording_to_hls2,recording_to_hls,pdf_to_thumbs,
+                  pdf_to_swf_thumbs,pdf_to_swf,pdf_to_pages,pdf_thumb,
+                  pdf_split_pages,image_thumb,doc_to_pdf_thumbs,
+                  doc_to_pdf_swf,doc_to_pdf,do_nothing,audio_to_mp3]).
