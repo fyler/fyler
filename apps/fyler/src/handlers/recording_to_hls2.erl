1647d31110af4e8befcc760d5e1b9349a8a4b524
silkbottle <silkbottle@gmail.com>
2014-09-12 15:29:58 +0400

    add redirect to other handlers (recording_to_hls2)

--- apps/fyler/src/handlers/recording_to_hls2.erl
+++ apps/fyler/src/handlers/recording_to_hls2.erl
@@ -12,43 +12,49 @@ category() ->
 
 run(File) -> run(File,[]).
 
-run(#file{tmp_path = Path, name = Name, dir = Dir}, _Opts) ->
+run(#file{tmp_path = Path, name = Name, dir = Dir} = File, _Opts) ->
   Start = ulitos:timestamp(),
   Playlist = Name ++ ".m3u8",
   Options = #{filename => Path, hls_writer => #{dir => Dir, playlist => Playlist}},
   #video_info{audio_codec = Audio, video_codec = Video} = video_probe:info(Path),
-  case convert(Audio, Video, Options) of
+  case convert(Audio, Video, Options, File) of
+    {ok, _} ->
+      {ok,#job_stats{time_spent = ulitos:timestamp() - Start, result_path = [list_to_binary(Playlist)]}};
     ok ->
       {ok,#job_stats{time_spent = ulitos:timestamp() - Start, result_path = [list_to_binary(Playlist)]}};
     {error, Reason} ->
       {error, Reason}
   end.
 
-convert("libspeex", "h264", Options) ->
+convert("libspeex", "h264", Options, File) ->
   Audio = #{input => #{codec => speex, channels => 1, sample_rate => 16000}, output => #{codec => aac, channels => 2}},
   AllOptions = Options#{audio => Audio},
   media_convert:flv_to_hls(AllOptions),
-  wait_result(AllOptions);
+  wait_result(AllOptions, File);
 
-convert("aac", "h264", Options) ->
+convert("aac", "h264", Options, File) ->
   media_convert:flv_to_hls(Options),
-  wait_result(Options);
+  wait_result(Options, File);
 
-convert("mp3", "h264", Options = #{hls_writer := HlsWriter}) ->
+convert("mp3", "h264", Options = #{hls_writer := HlsWriter}, File) ->
   AllOptions = Options#{hls_writer => HlsWriter#{audio => mp3}},
   media_convert:flv_to_hls(AllOptions),
-  wait_result(AllOptions);
+  wait_result(AllOptions, File);
 
-convert(_Audio, _Video, _Options) ->
-  {error, invalid_codecs}.
+convert(_Audio, "flashsv2", _Options, File) ->
+  recording_to_hls:run(File, [{stream_type, <<"share">>}]);
+
+convert(_Audio, _Video, _Options, File) ->
+  video_to_hls:run(File).
 
 
-wait_result(Options) ->
+wait_result(Options, File) ->
   receive
     {task_complete, Options} ->
       ok;
     {task_failed, Reason, Options} ->
-      {error, Reason}
+      ?D({"media_convert:flv_to_hls failed", Reason}),
+      video_to_hls:run(File)
   after 3600000 ->
     {error, timeout}
   end.
