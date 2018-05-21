d291c6f2586ed668abcc04114315f1823c65fce6
paderinandrey <paderinandrey2011@gmail.com>
2018-05-21 12:01:04 +0000

    refact 2

--- apps/fyler/src/openstack_cli.erl
+++ apps/fyler/src/openstack_cli.erl
@@ -8,16 +8,16 @@
 -export([instance/2, start_instance/2, stop_instance/2, ip_address_pattern/0]).
 
 instance(Id, Options) ->
-  CloudName = maps:get(name, Options, default),
-  os:cmd(io_lib:format("openstack --os-cloud=~s server show ~s --format=json", [CloudName, Id])).
+  os:cmd(io_lib:format("openstack --os-cloud=~s server show ~s --format=json", [cloud_name(Options), Id])).
 
 start_instance(Id, Options) ->
-  CloudName = maps:get(name, Options, default),
-  os:cmd(io_lib:format("openstack --os-cloud=~s server start ~s", [CloudName, Id])).
+  os:cmd(io_lib:format("openstack --os-cloud=~s server start ~s", [cloud_name(Options), Id])).
 
 stop_instance(Id, Options) ->
-  CloudName = maps:get(name, Options, default),
-  os:cmd(io_lib:format("openstack --os-cloud=~s server stop ~s", [CloudName, Id])).
+  os:cmd(io_lib:format("openstack --os-cloud=~s server stop ~s", [cloud_name(Options), Id])).
 
 ip_address_pattern() ->
   "\"addresses\": \"int-net1=(?<ip>[^\"]*),.*\"".
+
+cloud_name(Options) ->
+  maps:get(name, Options, default).
